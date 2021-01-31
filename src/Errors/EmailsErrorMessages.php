6a037e68d97383d4d89acc6aa5bf028c672d1d72
Christian Ramelow <info@codekandis.net>
2021-01-31 16:44:46 +0100

    Adds the enumeration `EmailsErrorMessages`.

---
+++ src/Errors/EmailsErrorMessages.php
@@ -0,0 +1,7 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\ClassListApi\Errors;
+
+abstract class EmailsErrorMessages
+{
+	public const EMAILS_UNKNOWN = 'The requested email does not exist.';
+}
