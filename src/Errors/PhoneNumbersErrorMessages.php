6e3091c03f83b577047d1256656610461d811994
Christian Ramelow <info@codekandis.net>
2021-01-31 16:44:23 +0100

    Adds the enumeration `PhoneNumbersErrorMessages`.

---
+++ src/Errors/PhoneNumbersErrorMessages.php
@@ -0,0 +1,7 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\ClassListApi\Errors;
+
+abstract class PhoneNumbersErrorMessages
+{
+	public const PHONE_NUMBER_UNKNOWN = 'The requested phone number does not exist.';
+}
