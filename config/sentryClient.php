f5b0d19b4d95852d26f64a29c51a1cde186998d9
Christian Ramelow <info@codekandis.net>
2021-01-31 19:39:28 +0100

    Splits the configuration `sentryClient`.

--- config/sentryClient.php
+++ config/sentryClient.php
@@ -1,11 +1,12 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\ClassListApi\Configurations;
 
+use const E_ALL;
+
 return [
 	'dsn'           => '',
 	'displayErrors' => false,
 	'errorTypes'    => E_ALL,
 	'environment'   => 'development',
-	'release'       => 'dev-development',
 	'serverName'    => 'api.classlist.codekandis'
 ];
